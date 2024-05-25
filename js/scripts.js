6a32e52b35bec11e749256bdb0e493f333802f4d
AnaisAhamada <168176449+AnaisAhamada@users.noreply.github.com>
2024-05-25 20:14:41 +0200

    Add files via upload

---
+++ js/scripts.js
@@ -0,0 +1,11 @@
+
+
+console.log("Hello console tab");
+
+//This is a single line comment, started with two slashes
+var title = 'Hello';
+/* This is a multi-line comment
+Everything in here has no effect.
+var test = 'This will not actually create a variable';
+*/
+var title2 = 'Hello?'; //Everything after the slashes has no effect
